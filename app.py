31210e8e3a5b12eac2cfff195ea5c0670d58c69d
sannaaaa45 <sannatirkey45@gmail.com>
2026-03-29 23:31:58 +0530

    updated run config for deployment

--- app.py
+++ app.py
@@ -306,4 +306,4 @@ def serve_index():
 if __name__ == '__main__':
     init_db()
     print("🚀 LeaveMate backend running on http://localhost:5000")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
