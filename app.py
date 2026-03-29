e307c4f5cda0697ff7312b56b24c025023bcb1a6
sannaaaa45 <sannatirkey45@gmail.com>
2026-03-30 00:10:42 +0530

    final fix port

--- app.py
+++ app.py
@@ -306,4 +306,6 @@ def serve_index():
 if __name__ == '__main__':
     init_db()
     print("🚀 LeaveMate backend running on http://localhost:5000")
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port=int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
